08edce92f2336cf65c2e432476f3d8507403be99
Kronos <dimamitru@gmail.com>
2024-01-04 16:04:08 +0200

    Setup Connection String and DBContext, Create SQL Tables and push Migrations

---
+++ book_worm_api/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace book_worm_api.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { 
+        }
+    }
+}
