20d251523f411c6e35c67781f1a6eea545534e12
Kronos <dimamitru@gmail.com>
2024-01-12 09:31:20 +0200

    Get Individual Menu Item

--- book_worm_api/Controllers/MenuItemController.cs
+++ book_worm_api/Controllers/MenuItemController.cs
@@ -25,5 +25,26 @@ namespace book_worm_api.Controllers
             _response.StatusCode = HttpStatusCode.OK;
             return Ok(_response);
         }
+
+        //Swagger makes difference between APIs by looking at tags and not at the function name and it's argument.
+        //If we have two instances of [HttpGet] tags, swagger will throw an error. The argument should be put in tag for it to make the diffence.
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetMenuItem(int id)
+        {
+            if(id == 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                return BadRequest(_response);
+            }
+            MenuItem menuItem = _db.MenuItems.FirstOrDefault(x => x.Id == id);
+            if (menuItem==null)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                return NotFound(_response);
+            }
+            _response.Result = menuItem;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
     }
 }
