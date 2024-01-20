ccb61c1ca25a602525ec4f95ba4d895ab4bb7e8f
Kronos <dimamitru@gmail.com>
2024-01-20 19:43:51 +0200

    Create/Update Order in Action

--- book_worm_api/Controllers/OrderController.cs
+++ book_worm_api/Controllers/OrderController.cs
@@ -1,5 +1,7 @@
 ﻿using book_worm_api.Data;
 using book_worm_api.Models;
+using book_worm_api.Models.Dto;
+using book_worm_api.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
@@ -70,6 +72,112 @@ namespace book_worm_api.Controllers
                 _response.Result = orderHeaders;
                 _response.StatusCode = HttpStatusCode.OK;
                 return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> CreateOrder([FromBody] OrderHeaderCreateDTO orderHeaderDTO)
+        {
+            try
+            {
+                OrderHeader order = new()
+                {
+                    ApplicationUserId = orderHeaderDTO.ApplicationUserId,
+                    PickupEmail = orderHeaderDTO.PickupEmail,
+                    PickupName = orderHeaderDTO.PickupName,
+                    PickupPhoneNumber = orderHeaderDTO.PickupPhoneNumber,
+                    OrderTotal = orderHeaderDTO.OrderTotal,
+                    OrderDate = DateTime.Now,
+                    StripePaymentIntentID = orderHeaderDTO.StripePaymentIntentID,
+                    TotalItems = orderHeaderDTO.TotalItems,
+                    Status = String.IsNullOrEmpty(orderHeaderDTO.Status) ? SD.status_pending : orderHeaderDTO.Status,
+                };
+
+                if (ModelState.IsValid)
+                {
+                    _db.OrderHeaders.Add(order);
+                    _db.SaveChanges();
+                    foreach (var orderDetailDTO in orderHeaderDTO.OrderDetailsDTO)
+                    {
+                        OrderDetails orderDetails = new()
+                        {
+                            OrderHeaderId = order.OrderHeaderId,
+                            ItemName = orderDetailDTO.ItemName,
+                            MenuItemId = orderDetailDTO.MenuItemId,
+                            Price = orderDetailDTO.Price,
+                            Quantity = orderDetailDTO.Quantity,
+                        };
+                        _db.OrderDetails.Add(orderDetails);
+                    }
+                    _db.SaveChanges();
+                    _response.Result = order;
+                    order.OrderDetails = null;
+                    _response.StatusCode = HttpStatusCode.Created;
+                    return Ok(_response);
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<ApiResponse>> UpdateOrderHeader(int id, [FromBody] OrderHeaderUpdateDTO orderHeaderUpdateDTO)
+        {
+            try
+            {
+                if (orderHeaderUpdateDTO == null || id != orderHeaderUpdateDTO.OrderHeaderId)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest();
+                }
+                OrderHeader orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.OrderHeaderId == id);
+
+                if (orderFromDb == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest();
+                }
+                if (!string.IsNullOrEmpty(orderHeaderUpdateDTO.PickupName))
+                {
+                    orderFromDb.PickupName = orderHeaderUpdateDTO.PickupName;
+                }
+                if (!string.IsNullOrEmpty(orderHeaderUpdateDTO.PickupPhoneNumber))
+                {
+                    orderFromDb.PickupPhoneNumber = orderHeaderUpdateDTO.PickupPhoneNumber;
+                }
+                if (!string.IsNullOrEmpty(orderHeaderUpdateDTO.PickupEmail))
+                {
+                    orderFromDb.PickupEmail = orderHeaderUpdateDTO.PickupEmail;
+                }
+                if (!string.IsNullOrEmpty(orderHeaderUpdateDTO.Status))
+                {
+                    orderFromDb.Status = orderHeaderUpdateDTO.Status;
+                }
+                if (!string.IsNullOrEmpty(orderHeaderUpdateDTO.StripePaymentIntentID))
+                {
+                    orderFromDb.StripePaymentIntentID = orderHeaderUpdateDTO.StripePaymentIntentID;
+                }
+                _db.SaveChanges();
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+
+
+
             }
             catch (Exception ex)
             {
--- book_worm_api/Models/Dto/OrderHeaderCreateDTO.cs
+++ book_worm_api/Models/Dto/OrderHeaderCreateDTO.cs
@@ -1,6 +1,25 @@
-﻿namespace book_worm_api.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace book_worm_api.Models.Dto
+
 {
     public class OrderHeaderCreateDTO
     {
+        [Required]
+        public string PickupName { get; set; }
+        [Required]
+        public string PickupPhoneNumber { get; set; }
+        [Required]
+        public string PickupEmail { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        public double OrderTotal { get; set; }
+
+
+        public string StripePaymentIntentID { get; set; }
+        public string Status { get; set; }
+        public int TotalItems { get; set; }
+
+        public IEnumerable<OrderDetailsCreateDTO> OrderDetailsDTO { get; set; }
     }
 }
